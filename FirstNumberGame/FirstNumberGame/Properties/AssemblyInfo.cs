c9728576bf847bac99e813719670119beba751de
Goutamkumar Kalburgi <goutamkumar.kalburgi@microfocus.com>
2019-11-22 13:40:30 +0530

    Number game using JS

---
+++ FirstNumberGame/FirstNumberGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FirstNumberGame")]
+[assembly: AssemblyDescription("Developed By: Goutamkumar Kalburgi")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Goutamkumar Kalburgi")]
+[assembly: AssemblyProduct("Goutamkumar Kalburgi")]
+[assembly: AssemblyCopyright("Goutamkumar Kalburgi")]
+[assembly: AssemblyTrademark("Goutamkumar Kalburgi")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c8268866-40cb-462e-aa9c-ee8b2681805a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1")]
+[assembly: AssemblyFileVersion("1")]
---
+++ FirstNumberGame/FirstNumberGame/callHTA.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstNumberGame
+{
+    class callHTA
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+                Directory.CreateDirectory(tempDirectory);
+
+                File.WriteAllText(tempDirectory + "\\" + "g1.hta", Properties.Resources.g1);
+
+                Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("FirstNumberGame.Resources.bg.jpg");
+                FileStream fileStream = new FileStream(tempDirectory + "\\" + "bg.jpg", FileMode.CreateNew);
+                for (int i = 0; i < stream.Length; i++)
+                    fileStream.WriteByte((byte)stream.ReadByte());
+                fileStream.Close();
+                Process p = new Process();
+                p.StartInfo.FileName = "mshta.exe";
+                p.StartInfo.Arguments = tempDirectory + "\\" + "g1.hta";
+                p.StartInfo.UseShellExecute = true;
+                p.Start();
+                p.WaitForExit();
+
+
+                Directory.Delete(tempDirectory, true);
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+
+
+
+        }
+    }
+}
